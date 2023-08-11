6d5e9ef1dc275d41b7e04d6558d930ea3429b7df
Alexander <DrA1ex@users.noreply.github.com>
2023-08-11 16:39:45 +0500

    Update example

--- src/examples/dashboard.ts
+++ src/examples/dashboard.ts
@@ -1,33 +1,34 @@
-import {Color, PlotAxisScale, PlotSeriesOverflow} from "../enum";
+import {Color, PlotAxisScale, PlotSeriesAggregationFn, PlotSeriesOverflow} from "../enum";
 import {MultiPlotChart} from "../multi-plot";
 
 const chart = new MultiPlotChart();
 
 chart.addPlot({xOffset: 0, yOffset: 0, width: 40, height: 31}, {
-    title: "overflow: skip"
+    title: "overflow: skip",
 });
 
 chart.addPlot({xOffset: 42, yOffset: 0, width: 60, height: 15}, {
-    title: "overflow: logScale",
+    title: "overflow: logScale (agg: max)",
     axisScale: PlotAxisScale.log,
 });
 
 chart.addPlot({xOffset: 42, yOffset: 16, width: 60, height: 15}, {
-    title: "overflow: linearScale",
-    axisScale: PlotAxisScale.logInverted
+    title: "overflow: linearScale (agg: mean)",
+    axisScale: PlotAxisScale.logInverted,
+    aggregation: PlotSeriesAggregationFn.mean
 });
 
 chart.addPlotSeries(0, {color: Color.red, overflow: PlotSeriesOverflow.skip});
-chart.addPlotSeries(1, {color: Color.blue, overflow: PlotSeriesOverflow.logScale});
+chart.addPlotSeries(1, {color: Color.blue, overflow: PlotSeriesOverflow.linearScale});
 chart.addPlotSeries(2, {color: Color.yellow});
 
 let delay = 66; // Delay in milliseconds
 
 for (let i = 0; i < 2000; i++) {
     setTimeout(() => {
-        chart.addSeriesEntry(0, 0, Math.cos(i * Math.PI / 15) - 2);
+        chart.addSeriesEntry(0, 0, Math.cos(i * Math.PI / 15));
         chart.addSeriesEntry(1, 0, Math.sin(i * Math.PI / 30) * 100 + 2);
-        chart.addSeriesEntry(2, 0, Math.sin(i * Math.PI / 30) * 100 + 2);
+        chart.addSeriesEntry(2, 0, Math.cos(i * Math.PI / 30) * 100 + 2);
 
         const chartData = chart.paint();
         console.clear();
